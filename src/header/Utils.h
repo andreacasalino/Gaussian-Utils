62fbb4a0353f0cea3ad5c9fdd11c36ba15a0a616
Andrea <andrecasa91@gmail.com>
2021-11-27 19:44:34 +0000

    generalizing compute mean and cov function

--- src/header/Utils.h
+++ src/header/Utils.h
@@ -13,38 +13,39 @@
 namespace gauss {
 constexpr double PI_GREEK = 3.14159;
 
-template <typename Iterable>
-Eigen::VectorXd computeMean(const Iterable &samples) {
+template <typename Iterable, typename SampleExtractor>
+Eigen::VectorXd computeMean(const Iterable &samples, const SampleExtractor& extractor) {
   if (samples.empty()) {
     throw Error("empty samples container, when computing mean");
   }
   Eigen::VectorXd result(samples.front().size());
   result.setZero();
   double coeff = 1.0 / static_cast<double>(samples.size());
-  for (const Eigen::VectorXd &sample : samples) {
-    result += coeff * sample;
+  for (const auto &sample : samples) {
+    result += coeff * extractor(sample);
   }
   return result;
 }
 
-template <typename Iterable>
-Eigen::MatrixXd computeCovariance(const Iterable &samples) {
+template <typename Iterable, typename SampleExtractor>
+Eigen::MatrixXd computeCovariance(const Iterable &samples, const SampleExtractor& extractor) {
   Eigen::MatrixXd temp;
-  return computeCovariance(samples, temp);
+  return computeCovariance(samples, temp, extractor);
 }
 
-template <typename Iterable>
+template <typename Iterable, typename SampleExtractor>
 Eigen::MatrixXd computeCovariance(const Iterable &samples,
-                                  Eigen::VectorXd &mean) {
+                                  Eigen::VectorXd &mean, const SampleExtractor& extractor) {
   if (samples.empty()) {
     throw Error("empty samples container, when computing covariance");
   }
-  mean = computeMean(samples);
+  mean = computeMean(samples, extractor);
   Eigen::MatrixXd result(samples.front().size(), samples.front().size());
   result.setZero();
   double coeff = 1.0 / static_cast<double>(samples.size());
-  for (const Eigen::VectorXd &sample : samples) {
-    result += coeff * (sample - mean) * (sample - mean).transpose();
+  for (const auto& sample : samples) {
+      const Eigen::VectorXd& sample_ref = extractor(sample);
+    result += coeff * (sample_ref - mean) * (sample_ref - mean).transpose();
   }
   return result;
 }
